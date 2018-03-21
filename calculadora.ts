b4f4a92faf0fa7c243d17efd58ae4ba941a765a5
alice.borges <alice.borges@outlook.com.br>
2018-03-21 09:30:16 -0300

    Adição de funções na calculadora

--- calculadora.ts
+++ calculadora.ts
@@ -6,9 +6,19 @@ function subtracao(num1:number, num2:number){
 	return num1-num2
 }
 
+function ambos(num1:number, num2:number){
+	return 'Soma: ' + soma(num1, num2) +'; Subtração: ' + subtracao(num1, num2)
+}
+
+function informaDados(num1, num2): string{
+	return 'informou ' + num1 + ' e ' + num2;
+}
+
 function calculadora (num1:number, num2:number, callback): number{
 	return callback(num1, num2)
 }
 
 console.log(calculadora(2, 4, soma))
 console.log(calculadora(2, 4, subtracao))
+console.log(calculadora(2, 4, ambos))
+console.log(calculadora(2, 4, informaDados))
