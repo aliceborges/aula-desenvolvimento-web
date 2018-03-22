547173f33985c0ed148f79ac40087ef0891f1d53
alice.borges <alice.borges@outlook.com.br>
2018-03-22 10:11:33 -0300

    Aula 22.01

---
+++ Aulas/ionic/primeiro-app/src/pages/hello-world/hello-world.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+// Export para utilizar nas outras páginas
+@Component({
+    selector: 'app-hello-world',
+    template: `<p>Hello World!</p>`
+
+})
+export class HelloWorldPage{
+
+}
--- Aulas/ionic/primeiro-app/src/pages/home/home.ts
+++ Aulas/ionic/primeiro-app/src/pages/home/home.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, IonicPage } from 'ionic-angular';
 
+//@IonicPage() // Serve para ser só página. CRTL+. para adicionar automaticamente nos Imports.
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController) {} //Injeção de dependência o public dentro do construtor - navCtrl vira atributo da classe
+  private menu (){
+    console.log('clicou no botão')
+    this.navCtrl.push('HelloWorldPage') //Push é para ir de uma página para outra
   }
 
 }
